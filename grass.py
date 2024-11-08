a704f42f7ed3ff6ce16890c5b82a632f0d3280e8
IDWR <35910302+IDWR2016@users.noreply.github.com>
2024-11-08 21:50:45 +0700

    Update grass.py

--- grass.py
+++ grass.py
@@ -11,15 +11,13 @@ from fake_useragent import UserAgent
 from loguru import logger
 import schedule
 import base64
-from playwright.async_api import async_playwright
 
 test_url = 'https://www.google.com'
-encoded_proxy_list_url = 'aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL3hwaG9iaWE0MDQvcHJveGllcy9tYWluL3NvY2tzNS50eHQ='
+encoded_proxy_list_url = 'aHR0cHM6Ly9hcGkucHJveHlzY3JhcGUuY29tL3YyLz9yZXF1ZXN0PWRpc3BsYXlwcm94aWVzJnByb3RvY29sPWh0dHAmdGltZW91dD0xMDAwMCZjb3VudHJ5PWFsbCZzc2w9YWxsJmFub255bWl0eT1hbGw='
 output_file = 'active_proxies.txt'
 user_ids_file = 'users.txt'
 proxy_list_url = base64.b64decode(encoded_proxy_list_url).decode()
 
-# Fungsi untuk mengecek proxy
 def check_proxy(proxy):
     try:
         session = requests.Session()
@@ -35,7 +33,6 @@ def check_proxy(proxy):
         logger.error(f"Error occurred while checking {proxy}: {e}")
         return None
 
-# Fungsi untuk menyimpan proxy yang aktif
 def save_active_proxies(proxy_list_url, output_file, max_proxies=2000):
     try:
         response = requests.get(proxy_list_url)
@@ -61,37 +58,9 @@ def save_active_proxies(proxy_list_url, output_file, max_proxies=2000):
         logger.error(f"Error terjadi pas ambil atau proses daftar proxy dari {proxy_list_url}: {e}")
         return []
 
-# Fungsi untuk scraping proxy baru
-async def scrape_proxies():
-    async with async_playwright() as p:
-        browser = await p.chromium.launch()
-        page = await browser.new_page()
-        await page.goto('https://free-proxy-list.net/')
+def log_reputation(proxy, completeness, consistency, timeliness, availability):
+    logger.info(f"Proxy: {proxy}, Kecompletan: {completeness}, Konsistensi: {consistency}, Waktu tepat: {timeliness}, Ketersediaan: {availability}")
 
-        # Mengambil data IP dan port
-        proxies = await page.evaluate('''() => {
-            const rows = Array.from(document.querySelectorAll('table tbody tr'));
-            return rows.map(row => {
-                const ip = row.querySelector('td:nth-child(1)')?.innerText;
-                const port = row.querySelector('td:nth-child(2)')?.innerText;
-                return { ip, port };
-            }).filter(proxy => proxy.ip && proxy.port);
-        }''')
-
-        # Menyimpan hasil ke file dengan pengecualian proxy di indeks 301 hingga 311
-        content = '\n'.join(
-            f'http://{proxy["ip"]}:{proxy["port"]}' 
-            for i, proxy in enumerate(proxies) 
-            if not (301 <= i + 1 <= 311)
-        )
-
-        with open(output_file, 'w') as file:
-            file.write(content)
-        print('Proxy list scraped and saved successfully.')
-
-        await browser.close()
-
-# Fungsi utama WebSocket dan koneksi ke WSS
 async def connect_to_wss(socks5_proxy, user_id, traffic_type='PET'):
     device_id = str(uuid.uuid3(uuid.NAMESPACE_DNS, socks5_proxy))
     logger.info(device_id)
@@ -101,7 +70,7 @@ async def connect_to_wss(socks5_proxy, user_id, traffic_type='PET'):
     while True:
         try:
             await asyncio.sleep(1)
-            custom_headers = {"User-Agent": random_user_agent, "Origin": "chrome-extension://caacbgbklghmpodbdafajbgdnegacfmo"}
+            custom_headers = {"User-Agent": random_user_agent}
             ssl_context = ssl.create_default_context()
             ssl_context.check_hostname = False
             ssl_context.verify_mode = ssl.CERT_NONE
@@ -110,7 +79,9 @@ async def connect_to_wss(socks5_proxy, user_id, traffic_type='PET'):
             server_hostname = "proxy.wynd.network"
             proxy = Proxy.from_url(socks5_proxy)
 
-            async with proxy_connect(uri, proxy=proxy, ssl=ssl_context, server_hostname=server_hostname, extra_headers=custom_headers) as websocket:
+
+            async with proxy_connect(uri, proxy=proxy, ssl=ssl_context, server_hostname=server_hostname,
+                                     extra_headers=custom_headers) as websocket:
                 async def send_ping():
                     while True:
                         send_message = json.dumps(
@@ -129,6 +100,13 @@ async def connect_to_wss(socks5_proxy, user_id, traffic_type='PET'):
                     message = json.loads(response)
                     logger.info(message)
 
+                    completeness = True
+                    consistency = True
+                    timeliness = True
+                    availability = True
+
+                    log_reputation(socks5_proxy, completeness, consistency, timeliness, availability)
+
                     if message.get("action") == "AUTH":
                         auth_response = {
                             "id": message["id"],
@@ -139,19 +117,25 @@ async def connect_to_wss(socks5_proxy, user_id, traffic_type='PET'):
                                 "user_agent": custom_headers['User-Agent'],
                                 "timestamp": int(time.time()),
                                 "device_type": "extension",
-                                "version": "1.0.10",
-                                "extension_id": "caacbgbklghmpodbdafajbgdnegacfmo"
+                                "version": "3.3.2"
                             }
                         }
-                        await websocket.send(json.dumps(auth_response))
-                        logger.debug(auth_response)
+                        try:
+                            await websocket.send(json.dumps(auth_response))
+                            logger.debug(auth_response)
+                        except Exception as e:
+                            logger.error(f"Gagal kirim respon AUTH: {e}")
 
                     elif message.get("action") == "PONG":
                         pong_response = {"id": message["id"], "origin_action": "PONG"}
-                        await websocket.send(json.dumps(pong_response))
-                        logger.debug(pong_response)
+                        try:
+                            await websocket.send(json.dumps(pong_response))
+                            logger.debug(pong_response)
+                        except Exception as e:
+                            logger.error(f"Gagal kirim respon PONG: {e}")
 
         except Exception as e:
+            pass
             await asyncio.sleep(10)
 
 async def main():
@@ -171,16 +155,10 @@ def perform_job():
     else:
         logger.error("Kagak ada proxy aktif, lewatin hubungan WebSocket.")
 
-# Memanggil scraping saat awal script berjalan
-asyncio.run(scrape_proxies())
-
-# Menjadwalkan scraping dan pemeriksaan proxy
-schedule.every(12).hours.do(lambda: asyncio.run(scrape_proxies()))
 schedule.every(24).hours.do(perform_job)
 
-perform_job()  # Menjalankan perform_job() saat awal script berjalan
+perform_job()
 
-# Loop penjadwalan
 while True:
     schedule.run_pending()
     time.sleep(1)
